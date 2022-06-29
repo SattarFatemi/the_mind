e6c88d2419a871c24cb7cda4bf247dd0073f118c
Arman <arman1381keshazar@gmail.com>
2022-06-29 20:53:23 +0430

    Client.sendRequest was added - & Response packages / their associated classes were created nothing but public final authToken was added

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -3,10 +3,12 @@ package Client;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.net.Socket;
 
 public class ClientMain {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8000);
+        Client client = new Client(socket);
     }
 }
